e71502bd27ce493380ccc731a5ba627193e61595
bibakroy <bibak68@student.sust.edu>
2022-03-03 19:04:37 +0600

    service section done

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="flex w-full justify-center items-center gradient-bg-services">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+        <div className="flex-1 flex flex-col justify-start items-start">
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient ">
+            Services that we
+            <br />
+            continue to improve
+          </h1>
+        </div>
+        <div className="flex-1 flex flex-col justify-start items-center">
+          <ServiceCard
+            color="bg-[#2952E3]"
+            title="Security Guaranteed"
+            icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+            subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our product"
+          />
+          <ServiceCard
+            color="bg-[#8945F8]"
+            title="Best Exchange Rate"
+            icon={<BiSearchAlt fontSize={21} className="text-white" />}
+            subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our product"
+          />
+          <ServiceCard
+            color="bg-[#F84550]"
+            title="Security Guaranteed"
+            icon={<RiHeart2Fill fontSize={21} className="text-white" />}
+            subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our product"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
