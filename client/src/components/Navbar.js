cd909ee490ed4e1794112834bc7bde305cf7f36b
bibakroy <bibak68@student.sust.edu>
2022-03-04 13:52:27 +0600

    transaction saction is almost done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,5 @@
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
-
-import logo from "./assets/images/logo.png";
 import { useState } from "react";
 
 const Navbar = () => {
